c5bc288c1425e2c8406167b66e63c3fc3c95d5fc
Ahmad Khaled Osman <158057636+Osman1899@users.noreply.github.com>
2024-04-24 03:23:35 +0000

    Add files via upload

---
+++ requete reseau/script.js
@@ -0,0 +1,42 @@
+let weather = {
+  apiKey: "af8442146c58b29918744b9ab7536c10",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, humidity, speed);
+    document.querySelector(".city").innerText = "Temperature à " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidité: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Vitesse du vent: " + speed + "km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        weather.search();
+    }
+})
